9e8bf563f1c3149ab4e73c5714fff353b70bd2e2
Adrian Martinez Rodriguez <adrianmrit@gmail.com>
2022-07-09 22:11:01 -0400

    refactor: move the program method to main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,28 +9,3 @@ use crate::tasks::ConfigFiles;
 
 pub mod args;
 pub mod tasks;
-
-pub fn program() -> Result<(), Box<dyn Error>> {
-    let args = YamisArgs::new(env::args());
-    return match args {
-        YamisArgs::CommandArgs(args) => {
-            let config_files = match args.file {
-                None => ConfigFiles::discover()?,
-                Some(file_path) => ConfigFiles::for_path(&file_path)?,
-            };
-            match args.task {
-                None => Err("No task to run was given.")?,
-                Some(task_name) => {
-                    let task_and_config = config_files.get_task(&task_name);
-                    match task_and_config {
-                        None => Err(format!("Task {task_name} not found."))?,
-                        Some((task, config)) => {
-                            task.run(&args.args, config)?;
-                            Ok(())
-                        }
-                    }
-                }
-            }
-        }
-    };
-}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
 use colored::Colorize;
 use std::env;
 use std::error::Error;
-use yamis::program;
+use yamis::args::YamisArgs;
 use yamis::tasks::{ConfigFile, ConfigFiles};
+
+/// Runs the program but returns errors. The main method should be
+/// the one to print the actual error.
+fn program() -> Result<(), Box<dyn Error>> {
+    let args = YamisArgs::new(env::args());
+    return match args {
+        YamisArgs::CommandArgs(args) => {
+            let config_files = match args.file {
+                None => ConfigFiles::discover()?,
+                Some(file_path) => ConfigFiles::for_path(&file_path)?,
+            };
+            match args.task {
+                None => Err("No task to run was given.")?,
+                Some(task_name) => {
+                    let task_and_config = config_files.get_task(&task_name);
+                    match task_and_config {
+                        None => Err(format!("Task {task_name} not found."))?,
+                        Some((task, config)) => {
+                            task.run(&args.args, config)?;
+                            Ok(())
+                        }
+                    }
+                }
+            }
+        }
+    };
+}
 
 fn main() {
     match program() {
