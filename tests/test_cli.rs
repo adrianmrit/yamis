7da95e4dcc225a391142704d82522ff4c291a552
Adrian Martinez Rodriguez <adrianmrit@gmail.com>
2022-07-12 17:51:19 -0400

    tests: test run os specific task and test run with environment

--- tests/test_cli.rs
+++ tests/test_cli.rs
@@ -39,3 +39,88 @@ fn test_run_simple_task() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn test_run_os_task() -> Result<(), Box<dyn std::error::Error>> {
+    let tmp_dir = TempDir::new().unwrap();
+    let mut file = File::create(tmp_dir.join("project.yamis.toml"))?;
+    file.write_all(
+        r#"
+    [tasks.hello.windows]
+    script = "echo hello windows"
+    
+    [tasks.hello.linux]
+    script = "echo hello linux"
+    
+    [tasks.hello.macos]
+    script = "echo hello macos"
+    
+    [tasks.hello_again]
+    script = "echo hello windows"
+    
+    [tasks.hello_again.linux]
+    script = "echo hello linux"
+    
+    [tasks.hello_again.macos]
+    script = "echo hello macos"
+    "#
+        .as_bytes(),
+    )?;
+
+    let expected = if cfg!(target_os = "windows") {
+        "hello windows"
+    } else if cfg!(target_os = "linux") {
+        "hello linux"
+    } else {
+        "hello macos"
+    };
+
+    let mut cmd = Command::cargo_bin("yamis")?;
+    cmd.current_dir(tmp_dir.path());
+    cmd.arg("hello");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(expected));
+
+    let mut cmd = Command::cargo_bin("yamis")?;
+    cmd.current_dir(tmp_dir.path());
+    cmd.arg("hello_again");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(expected));
+    Ok(())
+}
+
+#[test]
+fn test_set_env() -> Result<(), Box<dyn std::error::Error>> {
+    let tmp_dir = TempDir::new().unwrap();
+    let mut file = File::create(tmp_dir.join("project.yamis.toml"))?;
+    file.write_all(
+        r#"
+    [env]
+    greeting = "hello world"
+    one_plus_one = "two"
+    
+    [tasks.hello.windows]
+    script = "echo %greeting%, one plus one is %one_plus_one%"
+    
+    [tasks.hello]
+    script = "echo $greeting, one plus one is $one_plus_one"
+    
+    [tasks.hello.env]
+    greeting = "hi world"
+    
+    [tasks.hello.windows.env]
+    greeting = "hi world"
+    "#
+        .as_bytes(),
+    )?;
+
+    let mut cmd = Command::cargo_bin("yamis")?;
+    cmd.current_dir(tmp_dir.path());
+    cmd.arg("hello");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("hi world, one plus one is two"));
+    Ok(())
+}
